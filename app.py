492f394e7a2537b7cace80eab1ab1b4c7c29eda4
Tomislav Dukez <tomdu3@ymail.com>
2024-08-06 13:51:37 +0200

    add new routes

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+import os
 from flask import Flask, render_template
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
+# environment variables
+load_dotenv(override=True)
+
 app = Flask(__name__)
 
+# sqlalchemy setup and db connection
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -9,8 +20,39 @@ def index():
     Returns:
         template: index.html
     """
-    return render_template('base.html')
+    return render_template('index.html', todos=[{'task': 'Learn Flask', 'done': False}, {'task': 'Learn SQL', 'done': True}], title='Home')
+
+
+@app.route('/login')
+def login():
+    """login route
+
+    Returns:
+        template: login.html
+    """
+    return render_template('login.html', title='Login')
+
+
+@app.route('/about')
+def about():
+    """about route
+
+    Returns:
+        template: about.html
+    """
+    return render_template('about.html', title='About')
+
+@app.route('/signup')
+def signup():
+    """signup route
+
+    Returns:
+        template: signup.html
+    """
+    return render_template('signup.html', title='Signup')
+
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=os.getenv('DEBUG'))

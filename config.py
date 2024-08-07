4b1afe18323a74ffc3f1a33f27cf8e80122a4ac3
Tomislav Dukez <tomdu3@ymail.com>
2024-08-07 20:38:48 +0200

    fix break line over 79 ch

--- config.py
+++ config.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 # sqlalchemy setup and db connection
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('FLASK_ENV') == 'development'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = \
+    os.getenv('FLASK_ENV') == 'development'
 app.config['DEBUG'] = os.getenv('FLASK_ENV') == 'development'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 

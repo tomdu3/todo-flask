7daa68c30c6a83eb0d4944eafc0ac6f3e61a839e
Tomislav Dukez <tomdu3@ymail.com>
2024-08-16 12:53:58 +0200

    refactor app structure

--- app/static/js/script.js
+++
@@ -1,8 +0,0 @@
-// Auto-dismiss alerts with fade-out effect after 5 seconds
-$(document).ready(function() {
-    setTimeout(function() {
-      $('.alert').fadeOut('slow', function() {
-        $(this).alert('close');
-      });
-    }, 5000); // 5000 milliseconds = 5 seconds
-  });
